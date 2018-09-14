c842022a02c599898bc2c7697276a823d53ca176
douglas <ccydouglas327@hotmail.com>
2018-09-14 14:40:31 -0700

    data binding

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Dish } from '../shared/dish';
 
 const DISH = {
   name: 'Uthappizza',
@@ -49,8 +50,8 @@ const DISH = {
 })
 export class DishdetailComponent implements OnInit {
 
-  dish = DISH;
-  
+  @Input()
+  dish: Dish;
 
   constructor() { }
 
